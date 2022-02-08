575864e5214d8babfb82514839b30ddbedb89218
alemica97 <alessandro.micalizzi08@gmail.com>
2022-02-08 18:10:45 +0100

    salvo l'icona della X, in una costante JS

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//salvo in una costante l'elemento HTML che dovrò cliccare in seguito
+//salvo in una costante l'elemento HTML che dovrò cliccare in seguito per far apparire il menu
 const hamburgerIcon = document.querySelector('.header-right > a');
 console.log(hamburgerIcon);
 
@@ -11,3 +11,8 @@ hamburgerIcon.addEventListener('click', function(){
     console.log('prova');
     hamburgerMenu.classList.add('active');
 });
+
+//salvo l'elemento HTML che rappresenta la x che chiude il menù
+const closeIcon = document.querySelector('.hamburger-menu .close');
+console.log(closeIcon);
+
